d512ec400f2600e34c188092c4a0ec1a8095c525
Deepak Choudhary <deepak32145@gmail.com>
2024-04-29 16:21:11 +0530

    regex for phone numers and name

--- dc.js
+++ dc.js
@@ -1,17 +1,3 @@
-function testValue(value) {
-  const regex = /^(?!0\.00$)(?!0$)([1-9]\d*(\.\d{1,2})?|0\.\d*[1-9]\d*)$/;
-  if (regex.test(value)) {
-    console.log(value + " is a valid number.");
-  } else {
-    console.log(value + " is NOT a valid number.");
-  }
-}
+// ^\+?(\d[\s\-()]?){10,15}\d$
 
-// Testing different cases
-testValue("0.01"); // Valid: greater than 0, up to two decimal places
-testValue("1.23"); // Valid: greater than 0, up to two decimal places
-testValue("10"); // Valid: an integer greater than 0
-testValue("0.00"); // Invalid: exactly zero
-testValue("0"); // Invalid: zero
-testValue("00.00"); // Invalid: zero with two decimal places
-testValue("-1.23"); // Invalid: negative number
+//^[A-Za-z]+([\-'\s\.][A-Za-z]+)*$
