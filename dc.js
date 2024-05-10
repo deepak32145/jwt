c55b0d23d7f2922de70425b565b53030934de20a
Deepak Choudhary <deepak.choudhary03@ad.infosys.com>
2024-05-10 13:59:37 +0530

    check

--- dc.js
+++ dc.js
@@ -1,8 +1,8 @@
-const regex = /^\d+\.\d{2}$/;
-
-console.log("Testing '1.00':", regex.test("1.00")); // true
-console.log("Testing '10.00':", regex.test("10.00")); // true
-console.log("Testing '153.00':", regex.test("153.00")); // true
-console.log("Testing '1.2':", regex.test("1.2")); // false
-console.log("Testing '153.1':", regex.test("153.1")); // false
-console.log("Testing '153.100':", regex.test("153.100")); // false
+else if (answer.validation == 'numeric') {
+    this.questionForm?.addControl(
+      `${this.currentUseCaseId}_question_${question.questionId}_${answer.answerId}`,
+      this.formBuilder.control('', [
+        Validators.pattern(/^\d+\.\d{2}$/),
+      ])
+    );
+  }
